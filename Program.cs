9f42cc12b0ae15a2559782f698c6ca1b47d2add7
Mateusz Matlinski Pietrowski <mateusz@matlinski.com>
2025-04-04 22:18:39 +0200

    password hashing and input masking implemented

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Microsoft.Data.Sqlite;
+using static BCrypt.Net.BCrypt;
 
 var app = new App();
 
@@ -48,15 +49,34 @@ public class App{
         }
         return username;
     }
+    private string maskAndOutputUserInput(){
+        var password = string.Empty;
+        ConsoleKey key;
+        do {
+            var keyInfo = Console.ReadKey(intercept: true);
+            key = keyInfo.Key;
+
+            if (key == ConsoleKey.Backspace && password.Length > 0){
+                Console.Write("\b \b");
+                password = password[0..^1];
+            }
+            else if(!char.IsControl(keyInfo.KeyChar)) {
+                Console.Write("*");
+                password += keyInfo.KeyChar;
+            }
+        } while (key != ConsoleKey.Enter);
+        return password;
+    }
     private string validateAndOutputNewPassword(int attempt = 0){
         if(attempt > 3){
             Console.WriteLine("Too many failed attempts");
             System.Environment.Exit(1);
         }
         Console.WriteLine("Set your password");
-        string password = Console.ReadLine();
-        Console.WriteLine("Repeat the password");
-        string passwordConfirmation = Console.ReadLine();
+        string password = maskAndOutputUserInput();
+        Console.WriteLine("\nRepeat the password");
+        
+        string passwordConfirmation = maskAndOutputUserInput();
         if(password != passwordConfirmation){
             Console.WriteLine("Passwords don't match");
             attempt = attempt + 1;
@@ -90,10 +110,25 @@ public class App{
             validOptions
         );
         if(choice == 2){
-
             string validatedUsername = validateAndOutputUsername();
             string validatedPassword = validateAndOutputNewPassword();
-        }
+            string passwordHash = HashPassword(validatedPassword);
+            command = connection.CreateCommand();
+            command.CommandText = 
+                @"
+                INSERT INTO `user` (username, password)
+                VALUES ($username, $password)
+                "
+            ;
+            command.Parameters.AddWithValue(
+                "$username",
+                validatedUsername
+            );
+            command.Parameters.AddWithValue(
+                "$password",
+                passwordHash
+            );
+            command.ExecuteReader();        }
     }
 }
 
